ab0bb302c7150edc0d7b9447bdb5da322dab9987
Henrik Persson <henrik.l.persson@gmail.com>
2017-03-28 00:09:09 +0200

    Default dictionary collection malarky

--- mustangs/race.py
+++ mustangs/race.py
@@ -1,22 +1,34 @@
 #!/usr/bin/python
 
 import random
+from collections import defaultdict
 
-# Generate 25 horses (no 2 horses are alike) with times between 50 and 100
-horseList = [random.choice([i for i in xrange(50, 100)]) for r in xrange(25)]
+# Create a list of names to use as dictionary keys
+fh = open('horses.txt')
+lines = fh.readlines()
+names = [line.rstrip() for line in lines]
 
+# Generate times for horses (no 2 horses are alike) with 2-digit times (for easy printing)
+times = [random.choice([i for i in xrange(10, 99)]) for r in xrange(len(names))]
 
-def tournament(horsies) :
+# Initate the dictionaries
+horseTimes = dict(zip(names, times))
+database = defaultdict(dict)
 
-  # Group horses into heats of 5. Using list comprehension to create a list of lists
-  heats = [horsies[i:i+5] for i in range(0, len(horsies), 5)]
-  
-  # To the Races!
-  for heat in heats :
-    heat.sort()
+# Horrible hack for creating a heats of 5, where the last horse in each heat also competes in the next one
+heats = [[names[i] for i in range(j, j+5)] for j in range(0,len(names)-4,4)]
 
-  # Show the leaderboards
-  print(heats)
-  print(len(heats))
+# Qualifying races!
+for heat in heats :
+	race = zip([horseTimes[heat[i]] for i in range(0, len(heat))], heat)
+	race.sort()
 
-tournament(horseList)
+	# Create the first entries in the database
+	for position in range(5) :
+		for faster in range(0, position) :
+			database[race[position][1]][race[faster][1]] = True
+		for slower in range(position+1, 5) :
+			database[race[position][1]][race[slower][1]] = False
+
+print(str(database))
+
