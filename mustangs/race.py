704877e6282c84c2209aab74ebfead758b0cbc78
Henrik Persson <henrik.l.persson@gmail.com>
2017-03-28 18:42:55 +0200

    scoreboard update function

--- mustangs/race.py
+++ mustangs/race.py
@@ -3,6 +3,8 @@
 import random
 from collections import defaultdict
 
+numRaces = 0
+
 # Create a list of names to use as dictionary keys
 fh = open('horses.txt')
 lines = fh.readlines()
@@ -13,22 +15,48 @@ times = [random.choice([i for i in xrange(10, 99)]) for r in xrange(len(names))]
 
 # Initate the dictionaries
 horseTimes = dict(zip(names, times))
-database = defaultdict(dict)
+scoreboard = defaultdict(dict)
+
+def updateScoreboard() :
+	for horse in names :
+	 	for relation in scoreboard[horse].copy() :
+			inceptions = zip(scoreboard[relation].keys(), scoreboard[relation].values())
+			for inception in inceptions :
+				if scoreboard[horse][relation] == inception[1] :
+					scoreboard[horse][inception[0]] = inception[1]
+
+def getNumRelations() :
+	numRelations = 0
+	for horse in scoreboard :
+		numRelations += len(scoreboard[horse])
+	return numRelations
 
 # Horrible hack for creating a heats of 5, where the last horse in each heat also competes in the next one
-heats = [[names[i] for i in range(j, j+5)] for j in range(0,len(names)-4,4)]
+heats = [[names[i] for i in range(j, j+5)] for j in range(0,len(names)-4,4)] #FIXME superflous horses glued!
 
 # Qualifying races!
 for heat in heats :
 	race = zip([horseTimes[heat[i]] for i in range(0, len(heat))], heat)
 	race.sort()
+ 	numRaces += 1
 
-	# Create the first entries in the database
+	# Create the first entries in the scoreboard
 	for position in range(5) :
 		for faster in range(0, position) :
-			database[race[position][1]][race[faster][1]] = True
+			scoreboard[race[position][1]][race[faster][1]] = True
 		for slower in range(position+1, 5) :
-			database[race[position][1]][race[slower][1]] = False
+			scoreboard[race[position][1]][race[slower][1]] = False
 
-print(str(database))
+print(getNumRelations())
 
+# We now have a starting point where each horse has 4 or 8 relations
+# (the horses that ran in two heats have twice the relations)
+#
+# Go through the scoreboard and try to build additional relations
+updateScoreboard()
+updateScoreboard() # with the updated scoreboard run it again for even more relations (third time is not the charm, twice is enough!)
+
+#while len(names) * len(names) < getNumRelations() : # the scoreboard is not complete, need to run more races 
+	
+
+print(numRaces)
