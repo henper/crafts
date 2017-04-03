d3a905dd05ef49bd7413718c4e92d9f53d2267ef
Henrik Persson <henrik.l.persson@gmail.com>
2017-04-03 18:14:21 +0200

    New tactic (fubar)

--- mustangs/race.py
+++ mustangs/race.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
-import random
+#import random
 from collections import defaultdict
+import codecs
 
 numRaces = 0
 
@@ -10,7 +11,7 @@ numRaces = 0
 times = [107,47,102,64,50,100,28,91,27,5,22,114,23,42,13,3,93,8,92,79,53,83,63,7,15,66,105,57,14,65,58,113,112,1,62,103,120,72,111,51,9,36,119,99,30,20,25,84,16,116,98,18,37,108,10,80,101,35,75,39,109,17,38,117,60,46,85,31,41,12,29,26,74,77,21,4,70,61,88,44,49,94,122,2,97,73,69,71,86,45,96,104,89,68,40,6,87,115,54,123,125,90,32,118,52,11,33,106,95,76,19,82,56,121,55,34,24,43,124,81,48,110,78,67,59]
 
 # Create a list of names to use as dictionary keys
-fh = open('moreHorses.txt')
+fh = codecs.open('moreHorses.txt', encoding='utf-8')
 lines = fh.readlines()
 lines = lines[0:len(times)-1]
 names = [line.rstrip() for line in lines]
@@ -21,84 +22,83 @@ horseTimes = dict(zip(names, times))
 scoreboard = defaultdict(dict)
 
 def updateScoreboard() :
-	for horse in names :
-	 	for relation in scoreboard[horse].copy() :
-			inceptions = zip(scoreboard[relation].keys(), scoreboard[relation].values())
-			for inception in inceptions :
-				if scoreboard[horse][relation] == inception[1] :
-					scoreboard[horse][inception[0]] = inception[1]
+  for horse in names :
+     for relation in scoreboard[horse].copy() :
+      inceptions = zip(scoreboard[relation].keys(), scoreboard[relation].values())
+      for inception in inceptions :
+        if scoreboard[horse][relation] == inception[1] :
+          scoreboard[horse][inception[0]] = inception[1]
 
 def getNumRelations() :
-	numRelations = 0
-	for horse in scoreboard :
-		numRelations += len(scoreboard[horse])
-	return numRelations
+  numRelations = 0
+  for horse in scoreboard :
+    numRelations += len(scoreboard[horse])
+  return numRelations
 
 def findUnrelatedHorses( horseList ) :
-	unrelated = ['', '' ,'' ,'']
-	found = 0;
-	for horse in names :
-		if horse not in horseList :
-			unrelated[found] = horse
-			found += 1
-			if found == len(unrelated) :
-				return unrelated
-	#print('short race with only ' + str(found))
-	return unrelated[0:found]
+  unrelated = ['', '' ,'' ,'']
+  found = 0;
+  for horse in names :
+    if horse not in horseList :
+      unrelated[found] = horse
+      found += 1
+      if found == len(unrelated) :
+        return unrelated
+  #print('short race with only ' + str(found))
+  return unrelated[0:found]
 
 def race( heat ) :
-	global numRaces
-	numRaces += 1
-	race = zip([horseTimes[heat[i]] for i in range(0, len(heat))], heat)
-	race.sort()
-	return zip(*race)[1] # return a sorted tuple of the horses in the race 
+  global numRaces
+  numRaces += 1
+  race = zip([horseTimes[heat[i]] for i in range(0, len(heat))], heat)
+  race.sort()
+  return zip(*race)[1] # return a sorted tuple of the horses in the race 
 
 def resultToScoreboard( result ) :
-	global scoreboard
-	for position in range(len(result)) :
-		for faster in range(0, position) :
-			scoreboard[result[position]][result[faster]] = True
-		for slower in range(position+1, len(result)) :
-			scoreboard[result[position]][result[slower]] = False
+  global scoreboard
+  for position in range(len(result)) :
+    for faster in range(0, position) :
+      scoreboard[result[position]][result[faster]] = True
+    for slower in range(position+1, len(result)) :
+      scoreboard[result[position]][result[slower]] = False
 
 def ranking() :
-	ranking = range(len(scoreboard))
-	timeTbl = range(len(ranking))
-	for horse in scoreboard :
-		ranking[ sum(scoreboard[horse].values()) ] = horse
-	for rank in range(len(ranking)) :
-		timeTbl[rank] = horseTimes[ranking[rank]];
-	return timeTbl
+  ranking = range(len(scoreboard))
+  timeTbl = range(len(ranking))
+  for horse in scoreboard :
+    ranking[ sum(scoreboard[horse].values()) ] = horse
+  for rank in range(len(ranking)) :
+    timeTbl[rank] = horseTimes[ranking[rank]];
+  return timeTbl
 
-# Horrible hack for creating heats of 5, where the last horse in each heat also competes in the next one
-heats = [[names[i] for i in range(j, j+5)] for j in range(0,len(names)-4,4)] #FIXME superflous horses glued!
-
-# Qualifying races!
-for heat in heats :	
-	resultToScoreboard(race(heat))
-
-# We now have a starting point where each horse has 4 or 8 relations
-# (the horses that ran in two heats have twice the relations)
-#
-# Go through the scoreboard and try to build additional relations
-updateScoreboard()
-updateScoreboard() # with the updated scoreboard run it again for even more relations (third time is not the charm, twice is enough!)
+def printRaceResults( result ) :
+	for horse in result :
+		print()
 
 relationshipGoal = len(names) * (len(names) - 1)
 while getNumRelations() < relationshipGoal : # the scoreboard is not complete, need to run more races 
-	
-	for horse in scoreboard :	
-		if( len(scoreboard[horse]) < len(names) - 1 ) : # find the first horse which has an incomplete scoreboard
-			relations = scoreboard[horse].keys()
-			relations.append(horse)
-			final = findUnrelatedHorses( relations )
-			final.append(horse)
-			result = race(final)
-			print('New ranking: ' + str(result))
-			resultToScoreboard(race(final))
-			break
-	updateScoreboard()
-	#print(getNumRelations())
+  
+  # Sort the horses according to their connections
+  connections = [(len(scoreboard[horse]), horse) for horse in names]
+  connections.sort()
+
+  # Find the most connected, but not complete horse
+  for horseTuple in reversed(connections) :
+  	if horseTuple[0] < len(names) :
+  		mostConnectedNotCompleteHorse = horseTuple[1]
+  		break
+
+  # Race the most connected horse against the 4 least connected
+
+  result = race([connections[0][1],
+                 connections[1][1],
+                 connections[2][1],
+                 connections[3][1],
+                 mostConnectedNotCompleteHorse])
+  resultToScoreboard(result)
+  updateScoreboard()
+  print(getNumRelations())
+
 
 print('It took ' + str(numRaces) + ' five-horse-races to rank all ' + str(len(names)) + ' horses')
 print(str(ranking()))
