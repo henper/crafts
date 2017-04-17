5ca245dbe5ce8f60c4b525314cf2fb516886a23d
Henrik Persson <henrik.l.persson@gmail.com>
2017-04-17 21:46:15 +0200

    algo cleanup prep

--- mustangs/race.py
+++ mustangs/race.py
@@ -5,22 +5,29 @@ from collections import defaultdict
 import matplotlib.pyplot as plt
 import codecs
 
+from random import shuffle
+
 numRaces = 0
 
 # Generate times for horses (no 2 horses are alike) with 2-digit times (for easy printing)
 #times = [random.choice([i for i in xrange(10, 99)]) for r in xrange(len(names))]
 times = [107,47,102,64,50,100,28,91,27,5,22,114,23,42,13,3,93,8,92,79,53,83,63,7,15,66,105,57,14,65,58,113,112,1,62,103,120,72,111,51,9,36,119,99,30,20,25,84,16,116,98,18,37,108,10,80,101,35,75,39,109,17,38,117,60,46,85,31,41,12,29,26,74,77,21,4,70,61,88,44,49,94,122,2,97,73,69,71,86,45,96,104,89,68,40,6,87,115,54,123,125,90,32,118,52,11,33,106,95,76,19,82,56,121,55,34,24,43,124,81,48,110,78,67,59]
 
+#shuffle(times)
+
 # Create a list of names to use as dictionary keys
 fh = codecs.open('moreHorses.txt', encoding='utf-8')
 lines = fh.readlines()
 lines = lines[0:len(times)]
 names = [line.rstrip() for line in lines]
-horses = set(names)
 
 # Initate the dictionaries
 horseTimes = dict(zip(names, times))
 scoreboard = defaultdict(dict)
+
+# Initiate the sets
+horses = set(names)
+completed = set([])
 
 def updateScoreboard() :
   for horse in names :
@@ -71,18 +78,15 @@ def printRaceResults( result ) :
     print(horse + ': ' + str(horseTimes[horse]))
 
 def mostConnectedIn( horseList ) : #but not completely connected
-    # Sort the horses according to their connections
+  if horseList == set([]) :
+    return None
+
+  # Sort the horses according to their connections
   connections = [(len(scoreboard[horse]), horse) for horse in horseList]
   connections.sort()
   connections.reverse()
 
-  # Find the most connected, but not complete horse
-  for horseTuple in connections :
-    if horseTuple[0] < len(horses) - 1 :
-      return horseTuple[1]
-      break
-  #print(str(horseList) + 'only have completed connections')
-  return None
+  return connections[0][1]
 
 def mostConnectedAndUnrelatedIn( horseSet ) :
   goldenBoy = mostConnectedIn(horseSet)
@@ -100,7 +104,7 @@ fig.set_figheight(10)
 fig.set_figwidth(20)
 
 # Set up the canvas, the ranges here decides the elements to plot and y axis ranges (if not limited)
-xticks = 226 #max number of races
+xticks = 250 #max number of races
 yrange = [0 for i in range(xticks)]; yrange[len(yrange)-1] = relationshipGoal
 line = ax[0].plot(range(xticks), yrange, animated=True)
 bars = ax[1].bar(times, range(125), animated=True)
@@ -126,17 +130,13 @@ fig.canvas.draw()
 numRelations = 0; relationsList = [0 for i in range(xticks)]
 while numRelations < relationshipGoal : # the scoreboard is not complete, need to run more races 
   try :
-    # Plot stuff
-    paint = 'blue'
-    dot = 'o'
 
     # Actual algo.
-    intersection = horses
+    intersection = horses - completed
     lineup = ['', '', '', '', '']
     for position in range(len(lineup)) :
       lineup[position], intersection = mostConnectedAndUnrelatedIn(intersection)
       if lineup[position] == None :
-        paint = 'red'; dot = '*' # signify failiure to get a perfect race
         del(lineup[position:])
         break
 
@@ -144,7 +144,12 @@ while numRelations < relationshipGoal : # the scoreboard is not complete, need t
     #printRaceResults(result)
     resultToScoreboard(result)
     updateScoreboard()
-    #numRelations = getNumRelations() # calculated by plot stuff
+
+    # Keep a record of completed horses
+    for horse in horses.copy() :
+      if len(scoreboard[horse]) == len(horses) - 1 :
+        #horses -= set([horse])
+        completed.add(horse)
 
     # more plot stuff
 
