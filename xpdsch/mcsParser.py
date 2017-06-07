eb56a851ac9a2b345ce612a2b899dfaf5ee150bf
Henrik Persson <henrik.l.persson@gmail.com>
2017-06-07 10:38:30 +0200

    working(?) prototype

--- xpdsch/mcsParser.py
+++ xpdsch/mcsParser.py
@@ -24,20 +24,29 @@ import collections
 # lines = open(sys.argv[1])
 lines = open('test.log')
 
-# Named tpules as key-value pair in dicttionary
-Mismatch = collections.namedtuple('Mismatch', ['subframe', 'symbol', 'antenna', 'resourceElement'])
 
-mismatches = collections.deque([Mismatch])
+mismatches = collections.deque()
 
 for line in lines :
   mo = re.search('Mismatch for symbols_16\[(?P<sf>[0-9]*)\]\[(?P<symbol>[0-9]*)\]\[(?P<antenna>[0-9]*)\]\[(?P<re>[0-9]*)\] \((IM|RE)\), REF: (?P<ref>[-0-9]*), DUT: (?P<dut>[-0-9]*)', line)
   if mo :
-    Mismatch.subframe        = mo.group('sf')
-    Mismatch.symbol          = mo.group('symbol')
-    Mismatch.antenna         = mo.group('antenna')
-    Mismatch.resourceElement = mo.group('re')
+    # Named tpules as key-value pair in dicttionary
+    Mismatch = collections.namedtuple('Mismatch', ['subframe', 'symbol', 'antenna', 'element'])
+
+    Mismatch.subframe = mo.group('sf')
+    Mismatch.symbol   = mo.group('symbol')
+    Mismatch.antenna  = mo.group('antenna')
+    Mismatch.element  = mo.group('re')
 
     mismatches.append(Mismatch)
 
-for mismatch in mismatches :
-  print(mismatch.symbol)
+subframes = set([mismatch.subframe for mismatch in mismatches])
+symbols   = set([mismatch.symbol   for mismatch in mismatches])
+antennas  = set([mismatch.antenna  for mismatch in mismatches])
+elements  = set([mismatch.element  for mismatch in mismatches])
+
+print('Mismatches exist in the following: ')
+print('subframes : ' + ', '.join(str(subframe) for subframe in subframes))
+print('symbols   : ' + ', '.join(str(symbol)   for symbol   in symbols))
+print('antennas  : ' + ', '.join(str(antenna)  for antenna  in antennas))
+print('elements  : ' + ', '.join(str(element)  for element  in elements))
