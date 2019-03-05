805fef5883590b29f18a187039fed6adc3436f4b
eponhik <henrik.l.persson@ericsson.com>
2019-03-05 16:43:28 +0100

    simple block

--- myBlockChain/block.py
+++ myBlockChain/block.py
@@ -5,6 +5,7 @@ Created on Mar 5, 2019
 '''
 import unittest
 import hashlib
+import time
 
 class Block :
     prevHash = 0
@@ -12,19 +13,27 @@ class Block :
     data = ""
     timeStamp = 0
     
-    def __init__(self, prevHash, data) :
+    def __init__(self, data, prevHash=0) :
         self.prevHash = prevHash
-        self.thisHash = hashlib.sha256(data)
-
+        # concatenate data, prev hash, nonce and timestamp
+        concat = data + time.ctime() + str(prevHash) #TODO nonce
+        self.thisHash = hashlib.sha256(concat)
 
 class Test(unittest.TestCase):
 
 
-    def testName(self):
-        genisesBlock = Block(0, "Genisis")
-        assert(genisesBlock.prevHash == 0)
+    def testGenisis(self):
+        genises = Block("Genisis")
+        assert(genises.prevHash == 0)
         pass
 
+    def testNextBlock(self):
+        genises  = Block("Genisis")
+        nextGen  = Block("I am next", genises.thisHash)
+        nextNext = Block("I am nextNext", nextGen.thisHash)
+        
+        assert(nextGen.prevHash == genises.thisHash)
+        assert(nextNext.prevHash == nextGen.thisHash)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
