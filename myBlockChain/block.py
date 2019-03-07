e4c358d35d14e563365f00a18d62dfc0be7c1760
Henrik <henrik.l.persson@gmail.com>
2019-03-07 23:46:42 +0100

    replaced hashlib with cryptography

--- myBlockChain/block.py
+++ myBlockChain/block.py
@@ -4,14 +4,13 @@ Created on Mar 5, 2019
 @author: eponhik
 '''
 import unittest
-import hashlib # TODO replace with cryptography hash
-import time
 
 # note to self, by see about aquiring Gordon Freemans suite
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import dsa
 from cryptography.exceptions import InvalidSignature
+from time import ctime
 
 class Wallet:
     def __init__(self):
@@ -33,29 +32,32 @@ class Transaction:
         return True
 
 class Block :    
-    def __init__(self, prevHash, data="") :
+    def __init__(self, prevHash, data=b"") :
         self.prevHash = prevHash
-        self.timeStamp = time.ctime()
+        self.timeStamp = ctime().encode('utf-8')
         self.data = data
-        # concatenate data, prev hash, (nonce) and timestamp
-        concat = self.data + self.timeStamp + prevHash
-        self.thisHash = hashlib.sha256(concat.encode('utf-8')).hexdigest()
+        
+        digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+        digest.update(self.data)
+        digest.update(self.timeStamp)
+        digest.update(self.prevHash)
+        self.thisHash = digest.finalize()
 
 class BlockChain :
     def __init__(self):
-        self.blocks = [Block('0')]
+        self.blocks = [Block(b'0')]
         self.length = 1
     def __len__(self):
         return self.length
     def __getitem__(self,key):
         return self.blocks[key]
-    def add(self, data=""):
+    def add(self, data=b""):
         lastIndex = len(self.blocks) - 1
         prevHash  = self.blocks[lastIndex].thisHash
         self.blocks.append(Block(prevHash, data))
         self.length = len(self.blocks)
     def validate(self):
-        prevHash = '0'
+        prevHash = b'0'
         for block in self.blocks:
             if block.prevHash != prevHash :
                 return False
@@ -73,14 +75,13 @@ class Test(unittest.TestCase):
 
 
     def testGenisis(self):
-        genises = Block('0', "Genisis")
-        assert(genises.prevHash == '0')
-        pass
+        genises = Block(b'0', b"Genisis")
+        assert(genises.prevHash == b'0')
 
     def testNextBlock(self):
-        genises  = Block('0', "Genisis")
-        nextGen  = Block(genises.thisHash, "I am next")
-        nextNext = Block(nextGen.thisHash, "I am nextNext")
+        genises  = Block(b'0', b"Genisis")
+        nextGen  = Block(genises.thisHash, b"I am next")
+        nextNext = Block(nextGen.thisHash, b"I am nextNext")
         
         print(nextGen.thisHash)
         print(nextNext.thisHash)
@@ -94,7 +95,7 @@ class Test(unittest.TestCase):
         assert(blockChain.validate())
         
         # tamper with the chain
-        blockChain.blocks[1] = Block(blockChain[0].thisHash, "All coins to Henrik")
+        blockChain.blocks[1] = Block(blockChain[0].thisHash, b"All coins to Henrik")
         assert(blockChain.validate() == False)
         
     def testWallet(self):
