60afb2147f670bdefbfc94117a49ef8de6c2b4b7
Henrik <henrik.l.persson@gmail.com>
2020-01-03 01:55:29 +0100

    fixed about a thousand bugs (in a hundred lines of code)

--- vasttrafik/esp8226/sketch_jan02a/sketch_jan02a.ino
+++ vasttrafik/esp8226/sketch_jan02a/sketch_jan02a.ino
@@ -2,61 +2,89 @@
 #include<ESP8266WiFiMulti.h>
 #include<ESP8266HTTPClient.h>
 
+#include <ArduinoJson.h>
+#include <string.h>
+
 const char* ssid = "biggie"; //your WiFi Name
 const char* password = "lusky_812";  //Your Wifi Password
 
 const char* client_credentials = "NWpiZkVaQkRNQUpWUGpOWWFaUUZkQWl3aGZnYTpkOGlQVHJyUTNDZ2tmY21HcGNBRmw0NXBmU2Nh";
-const char* device_id = "1";
+//const char* device_id = "1";
 
+
+/* Fetch with command:
+ * > curl --verbose -k -d "grant_type=client_credentials" -H "Authorization: Basic NWpiZkVaQkRNQUpWUGpOWWFaUUZkQWl3aGZnYTpkOGlQVHJyUTNDZ2tmY21HcGNBRmw0NXBmU2Nh" https://api.vasttrafik.se:443/token
+ *
+ * After wich the server certificate (starts with -----BEGIN CERTIFICATE----- and ends with -----END CERTIFICATE-----)
+ * saved to the file 'cert.perm'. Command below used to create finger print from certificate:
+ * > openssl x509 -noout -in ./cert.perm -fingerprint -sha1
+ */
 const uint8_t finger_print[] = {0x7A, 0xF1, 0x5B, 0xEF, 0xDA, 0xF8, 0x14, 0xFA, 0x0D, 0x65, 0x38, 0x17, 0xD2, 0xC4, 0x5F, 0xE3, 0x76, 0x0B, 0x6F, 0xBD};
 
 ESP8266WiFiMulti wifiMulti;
 
-char validToken[1024] = "";
+static char* validToken = NULL;
+static unsigned long expiration = 0;
 char* getValidToken()
 {
-  static int timestamp = 0;
-  static int expiration = 0;
-
-  if(millis() > timestamp + expiration)
+  // check if we already have a valid token
+  if (millis() > expiration)
   {
     Serial.println("Fetching new token..");
 
+    // if there was a previous token it has now expired and we can delete it
+    if(validToken != NULL)
+      free(validToken);
+
     std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
-    client->setFingerprint(finger_print);
+    client->setFingerprint(finger_print); //TODO: figure out how to fetch the certificate and create fingerprint programmatically
     HTTPClient https;
+
+    if (!https.begin(*client, "https://api.vasttrafik.se/token"))
+      return NULL; // TODO print SSL error?
     
-    //if(https.begin(*client, "https://api.vasttrafik.se", 443))
-    if(https.begin(*client, "193.183.128.134", 443))
+    https.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    https.addHeader("Authorization", String("Basic ") + String(client_credentials));
+    int httpCode = https.POST("grant_type=client_credentials&scope=1");  //device_id
+
+    if (httpCode != 200)
     {
-      Serial.println("Connected to server");
-
-      https.addHeader("Content-Type", "application/x-www-form-urlencoded");
-      https.addHeader("Authorization", String("Basic ") + String(client_credentials));
-      int httpCode = https.POST("/token");
-
-      Serial.print("POST http-code: "); Serial.println(httpCode);
-      
-      Serial.println("Response:");
-      String payload = https.getString();
-      Serial.println(payload);
-
-      https.end();
-      Serial.println("\nDisconnected");
-
-    }
-    else
-    {
-      Serial.println(String("Connection to server failed"));
+      Serial.println(String("HTTP Return code: ") + httpCode);
+      return NULL;
     }
 
-    Serial.print("last ssl error: "); Serial.println(client->getLastSSLError());
-  
-    //char* buf[2048];
-    //client->getLastSSLError(buf, sizeof(buf));
-    //Serial.println(buf);
+    const size_t capacity = 256; // arbitrarily chosen...
+    DynamicJsonDocument json(capacity);
+    String responseStr = https.getString(); //TODO use getStream() instaed
+    Serial.println(responseStr);
+    DeserializationError response = deserializeJson(json, responseStr);
 
+    if (response != DeserializationError::Ok)
+    {
+      Serial.println(response.c_str());
+      return NULL;
+    }
+
+    // fetch and save the token on the heap
+    const char* tempToken = json["access_token"];
+    if (tempToken == NULL)
+    {
+      Serial.println("Response did not include a token?");
+      return NULL;
+    }
+
+    validToken = (char*)malloc(strlen(tempToken) + 1);
+    strcpy(validToken, tempToken);
+
+    unsigned long validity = json["expires_in"];
+    expiration = millis() + validity * 1000;
+
+    https.end();
   }
+
+  Serial.print("token: "); Serial.println(validToken);
+  Serial.print("expires in: "); Serial.println(expiration);
+
   return validToken;
 }
 
@@ -79,19 +107,19 @@ void setup()
   }
   Serial.println("");
   Serial.println("Connected!");
-
-  char* token = getValidToken();
   
 }
 
 void loop()
 {
-  if(wifiMulti.run() != WL_CONNECTED)
+  if (wifiMulti.run() != WL_CONNECTED)
     return;
   
   Serial.println("Heartbeat!");
   digitalWrite(LED_BUILTIN, HIGH);
   delay(2000);
+
+  char* token = getValidToken();
 
   digitalWrite(LED_BUILTIN, LOW);
   delay(2000);
