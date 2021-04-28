bb28318e308ce155fc553ce3a6dd57d620d6620c
Henrik Persson <henrik.l.persson@gmail.com>
2021-04-28 22:56:57 +0200

    add brightness adjustment to magic mirror sensor process

--- magicMirror/motionDetector.py
+++ magicMirror/motionDetector.py
@@ -8,15 +8,33 @@ hue.connect()
 
 # state
 monitorOn = True
+brightness = 0
 
 # shell commands for Rpi to enable and disable video output
-videoOn  = 'vcgencmd display_power 1; echo "Display On"'
-videoOff = 'vcgencmd display_power 0; echo "Display Off"'
+videoOn  = 'ddcutil setvcp D6 01 --noverify'
+videoOff = 'ddcutil setvcp D6 05 --noverify'
+
+# shell commands for Rpi to adjust brightness with i2c over HDMI
+setBrightness = 'ddcutil setvcp 10 '
 
 # assume x86 for host, and arm for Pi. This won't hold forever :)
 if os.uname()[4].startswith('x86'):
     videoOn  = 'echo "Display On"'
     videoOff = 'echo "Display Off"'
+    setBrightness = 'echo "Adjusting brightness"'
+
+def adjustBrightness():
+    global brightness
+
+    # Simple linear ambient to brightness, max brightness at 20000, min at 0.
+    ambient = hue.get_sensor(sensor_id=7, parameter='state')['lightlevel']
+    k = 200
+    setpoint = min(100, int(ambient/k))
+
+    if setpoint != brightness:
+        brightness = setpoint
+        os.system(setBrightness + str(brightness) + ' --noverify') # saw an error regarding max retries, author suggested no verify (used to be default)
+        os.system('echo "Ambient at ' + str(ambient) + ' Brightness set to: ' + str(brightness) + '"') # log to stdout
 
 def main():
     global monitorOn, host
@@ -29,12 +47,13 @@ def main():
             # change state
             monitorOn = presence
 
-            if monitorOn is True:
+            if monitorOn:
                 os.system(videoOn)
             else:
                 os.system(videoOff)
 
         if monitorOn:
+            adjustBrightness()
             time.sleep(8)
         else:
             time.sleep(2)
