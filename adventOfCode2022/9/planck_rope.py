4f49b7391754e85abfcd403cf5823875f342d6fe
Henrik Persson <henrik.persson@plejd.com>
2022-12-09 11:26:49 +0100

    Advents Calendar 2022-12-08, part 2 (wip)

--- adventOfCode2022/9/planck_rope.py
+++ adventOfCode2022/9/planck_rope.py
@@ -1,40 +1,42 @@
 import numpy as np
 
-head = np.array([0, 0])
-tail = np.array([0, 0])
+rope = np.array([[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])
 
-history_of_the_tail = np.array([tail])
+history_of_the_tail = np.array([[0, 0]])
 
-with open('./adventOfCode2022/9/input.txt', 'r') as input:
+with open('./adventOfCode2022/9/example.txt', 'r') as input:
     for line in input:
         direction, distance = line.split()
 
         match direction:
-            case 'U':
-                delta_dir = np.array([ 0,  1])
-            case 'D':
-                delta_dir = np.array([ 0, -1])
-            case 'R':
-                delta_dir = np.array([ 1,  0])
-            case 'L':
-                delta_dir = np.array([-1,  0])
+            case 'U': delta_dir = np.array([ 0,  1])
+            case 'D': delta_dir = np.array([ 0, -1])
+            case 'R': delta_dir = np.array([ 1,  0])
+            case 'L': delta_dir = np.array([-1,  0])
 
         for step in range(1, 1+int(distance)):
-            head += delta_dir
 
-            # not adjacent if any coordinate distance is 2
-            if np.any(2 == abs(head - tail)):
+            # move the head of the rope
+            rope[0] += delta_dir
 
-                # tail should move towards head
-                move = head - tail
+            # traverse from the tail end of the rope
+            for knot in range(1, 10):
+                head = rope[knot-1]
+                tail = rope[knot]
 
-                # but only by one step in either direction
-                # find the coordinate, x or y, that is two steps away
-                c = np.where(2 == abs(head-tail))[0][0]
-                move[c] /= 2 # and make it one
+                # not adjacent if any coordinate distance is 2
+                if np.any(2 == abs(head - tail)):
 
-                tail += move
+                    # tail should move towards head
+                    move = head - tail
 
-                history_of_the_tail = np.concatenate((history_of_the_tail, [tail]))
+                    # but only by one step in either direction
+                    # find the coordinate, x or y, that is two steps away
+                    c = np.where(2 == abs(head-tail))[0][0]
+                    move[c] /= 2 # and make it one
+
+                    rope[knot] += move
+
+            history_of_the_tail = np.concatenate((history_of_the_tail, [rope[-1]]))
 
     print(np.unique(history_of_the_tail, axis=0).shape[0])
