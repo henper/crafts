ade62fd679b9f21bded172dbeda065f147bf2055
Henrik Persson <henrik.persson@plejd.com>
2022-12-15 02:04:05 +0100

    Advents Calendar 2022-12-14(#2)

--- adventOfCode2022/14/sand_glass.py
+++ adventOfCode2022/14/sand_glass.py
@@ -1,28 +1,54 @@
 import pygame
 from time import sleep
 
-SCALING = 10
-OFFSET = 450
+SCALING = 4
 
 scan_lines = []
 with open('./adventOfCode2022/14/input.txt', 'r') as cave:
     for rock in cave:
+
         # list comprehension inception for enhanced legibility
-        line = [((int(x)-OFFSET)*SCALING,(int(y))*SCALING) for x,y in [c.split(',') for c in rock.split()[::2]]]
+        line = [(int(x),int(y)) for x,y in [c.split(',') for c in rock.split()[::2]]]
         scan_lines.append(line)
-
-WIDTH, HEIGHT = 125*SCALING, 200*SCALING
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-ROCK = (255, 255, 255)
-SAND = (210, 170, 110)
 
 sand = []
 rocks = []
 
 for rock in scan_lines:
-        for point in range(1, len(rock)):
-            rocks.append((rock[point-1], rock[point])) # save line segment
+    for point in range(1, len(rock)):
+        rocks.append((rock[point-1], rock[point])) # save line segment
+
+floor = max([max(rock, key=lambda y:y[1]) for rock in rocks], key=lambda y:y[1])[1] + 2
+
+right = max([max(rock, key=lambda x:x[0]) for rock in rocks], key=lambda x:x[0])[0]
+left  = min([min(rock, key=lambda x:x[0]) for rock in rocks], key=lambda x:x[0])[0]
+width = (right - left) + 400
+width += width % 2 #make even for offset
+
+OFFSET = int(width/2) + 1
+
+def scale(p):
+    x,y=p
+    x = ((x - 500) + OFFSET) * SCALING
+    y = y * SCALING
+    return (x,y)
+
+scaled_rocks = []
+for rock in rocks:
+    p1, p2 = rock
+    scaled_rocks.append((scale(p1), scale(p2)))
+rocks = scaled_rocks
+
+# floor
+rocks.append(((0, floor*SCALING), (width*SCALING, floor*SCALING)))
+
+HEIGHT = floor * SCALING
+screen = pygame.display.set_mode((width * SCALING, (1+floor) * SCALING))
+static = screen.copy()
+
+ROCK = (255, 255, 255)
+SAND = (210, 170, 110)
+BACK = (  0,   0,   0)
 
 def collides(grain : pygame.Rect):
     for rock in rocks:
@@ -34,43 +60,33 @@ def collides(grain : pygame.Rect):
 
     return False
 
-for rock in scan_lines:
-        for point in range(1, len(rock)):
-            pygame.draw.line(screen, ROCK, rock[point-1], rock[point], width=SCALING)
+# fill in static background
+for rock in rocks:
+    pygame.draw.line(static, ROCK, rock[0], rock[1], width=SCALING)
+
+screen.blit(static, (0,0))
 pygame.display.flip()
-input()
+#screen.fill(BACK)
 
 abyss = False
-while not abyss:
-
-    """
-    event = pygame.event.wait() # sleep until the user acts
-    # Allow for ways to exit the application
-    if event.type == pygame.QUIT: # closing the (proverbial) window
-        break
-    if event.type == pygame.KEYDOWN and event.key == pygame.K_c and pygame.key.get_mods() & pygame.KMOD_CTRL:
-        break
-    """
-
-    # draw rocks
-    for rock in scan_lines:
-        for point in range(1, len(rock)):
-            pygame.draw.line(screen, ROCK, rock[point-1], rock[point], width=SCALING)
-
-    # draw sand at rest
-    for grain in sand:
-        pygame.draw.circle(screen, SAND, grain.center, SCALING/2)
+blocked = False
+while not abyss and not blocked:
 
     # create hitbox for the new grain of sand
-    x,y = ((500 - OFFSET) * SCALING, 0)
+    x,y = (OFFSET * SCALING, 0)
     grain = pygame.Rect(0,0,SCALING,SCALING)
     grain.center = (x,y)
 
-    while True:
+    # blocked?
+    if collides(grain):
+        blocked = True
+
+    while not blocked:
 
         # fall, if possible
         dy = 1
         dx = 0 # straight
+
         if collides(grain.move((dx*SCALING, dy*SCALING))):
             dx = -1 # left
             if collides(grain.move((dx*SCALING, dy*SCALING))):
@@ -83,20 +99,23 @@ while not abyss:
             sand.append(grain)
             break # at rest
 
-        pygame.draw.circle(screen, (0,0,0), grain.center, SCALING)
-
-        grain = grain.move((dx*SCALING, dy*SCALING))
-
-        pygame.draw.circle(screen, SAND, grain.center, SCALING)
-        pygame.display.flip()
-
-        if grain.center[1] > HEIGHT:
+        if grain.center[1] > floor * SCALING:
             abyss = True
             break
 
-        #sleep(0.002)
+        grain = grain.move((dx*SCALING, dy*SCALING))
+
+        """ animate flowing sand
+        screen.blit(static, (0,0))
+        pygame.draw.circle(screen, SAND, grain.center, SCALING/2)
+        pygame.display.flip()
+        screen.fill(BACK) """
 
 
+    # add the stationary grain of sand to static background
+    pygame.draw.circle(screen, SAND, grain.center, SCALING/2)
+    if len(sand) % 100:
+        pygame.display.flip()
 
 print(len(sand))
 
