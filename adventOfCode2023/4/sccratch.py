56fb4de469e15c80191414842f9185d7233352cc
Henrik Persson <henrik.persson@plejd.com>
2023-12-04 08:05:31 +0100

    aoc 4 2 WIP

--- adventOfCode2023/4/sccratch.py
+++ adventOfCode2023/4/sccratch.py
@@ -1,15 +1,23 @@
 import re
 total_points = 0
 
-with open('./adventOfCode2023/4/input.txt', 'r') as cards:
+winnings = [1]
+cards_scratched = 0
+
+with open('./adventOfCode2023/4/example.txt', 'r') as cards:
 
     for card in cards:
+        cards_scratched = cards_scratched + 1
+        copies = winnings.pop(0)
+
+        if (copies == 1):
+            break
 
         numbers = re.findall('[0-9]+', card)
 
         game_no = numbers[0]
-        winning = numbers[1:11] # Note: example has 5 winning numbers
-        numbers = numbers[11:]
+        winning = numbers[1:6] # Note: example has 5 winning numbers, input  has 10
+        numbers = numbers[6:]
 
         winners = 0
         for number in numbers:
@@ -17,7 +25,14 @@ with open('./adventOfCode2023/4/input.txt', 'r') as cards:
                 winners = winners + 1
 
         if (winners):
-            points = pow(2, (winners-1))
-            total_points = total_points + points
+            new_cards = [1+copies] * winners
 
-print(total_points)
+            duplicates = min(len(winnings), len(new_cards))
+            for i in range(duplicates):
+                winnings[i] = winnings[i] + new_cards.pop(0)
+
+            winnings = winnings + new_cards
+
+
+
+print(cards_scratched)
