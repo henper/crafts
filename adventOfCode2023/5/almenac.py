509df2a2639d8aa36440074fa9911b8b4bf19346
Henrik Persson <henrik.persson@plejd.com>
2023-12-05 08:03:32 +0100

    aoc 5 2 WIP

--- adventOfCode2023/5/almenac.py
+++ adventOfCode2023/5/almenac.py
@@ -11,27 +11,56 @@ maps = [
     humidity_to_location
 ]
 
-nesting_doll = []
+locations = []
 
-for seed in seeds:
-
-    value = seed
-
-    nesting_doll.append([])
-    nesting_doll[-1].append(value)
-
+def traverse_mapping(value, maps):
     for map in maps:
+        for mapping in map:
+            destination, source, map_length = mapping
+            source_end = source + map_length
 
-        for tuple_range in map:
-            destination, source, length = tuple_range
+            start, length = value
+            end = start + length
 
-            if value in range(source, source+length):
-                value = destination + (value - source)
+            # check if completely covered
+            if start >= source and end <= (source + map_length):
+                value = (destination + (start - source), length)
                 break
 
-        nesting_doll[-1].append(value)
+            # check if partly covered, if so split in two value ranges
+            # where the part that gets translated spins up a new "thread"
+            # and the remainder remains here to be finished in the next iteration
+            remaining_maps = maps[(maps.index(map)+1):]
 
-nesting_doll = np.array(nesting_doll)
-locations = nesting_doll[:,-1]
+            # middle part
+            if start < source and end > (source + map_length):
+                traverse_mapping((start, source - start), remaining_maps)
+                traverse_mapping((source_end + 1, end - source_end - 1), remaining_maps)
+
+                value = (destination, map_length)
+                break
+
+            # bottom part of the values are covered by the mapping
+            if start >= source and start <= source_end:
+
+                traverse_mapping((destination + (start - source), source_end + 1 - start), remaining_maps)
+                value = (source + map_length, end - source_end + 1)
+                break
+
+            # top part
+            if end >= source and end <= source_end:
+                traverse_mapping((destination, end - source), remaining_maps)
+                value = (start, source - start)
+                break
+
+
+
+
+
+    locations.append(value[0])
+
+
+for seed in seeds:
+    traverse_mapping(seed, maps)
 
 print(min(locations))
