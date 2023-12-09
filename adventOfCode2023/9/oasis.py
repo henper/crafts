22b57825cfd41b5dbf87cac42ef7209b7b2e90d6
Henrik Persson <henrik.persson@plejd.com>
2023-12-09 09:44:33 +0100

    aoc 9 1

---
+++ adventOfCode2023/9/oasis.py
@@ -0,0 +1,22 @@
+from input import histories
+
+def extrapolate_underlying_change(progression : list[int]) -> int:
+
+    # Generate the underlying change
+    underlying_change = []
+    for index, val in enumerate(progression[1:]):
+        underlying_change.append(val - progression[index])
+
+    # Check if all zeroes
+    if all(0 for c in underlying_change):
+        return 0
+
+    return underlying_change[-1] + extrapolate_underlying_change(underlying_change)
+
+
+answer = 0
+for history in histories:
+    underlying_change = extrapolate_underlying_change(history)
+    answer += history[-1] + extrapolate_underlying_change(history)
+
+print(answer)
