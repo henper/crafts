4fd3f441bc936c55846cbf6b4713f9d10b6000c4
Henrik Persson <henrik.persson@plejd.com>
2023-12-15 15:01:05 +0100

    aoc 15 2

--- adventOfCode2023/15/hash.py
+++ adventOfCode2023/15/hash.py
@@ -1,4 +1,5 @@
 from input import sequence
+from collections import OrderedDict
 
 def hash(instruction: str):
     val = 0
@@ -10,8 +11,32 @@ def hash(instruction: str):
     return val
 
 
-answer = 0
+boxes = [OrderedDict() for i in range(256)]
+
 for instruction in sequence:
-    answer += hash(instruction)
+    # split the instruction into label, operation and possibly focal strength
+    addition = instruction.split('=')
+    if len(addition) == 2:
+        label, focal = addition
+        boxes[hash(label)][label] = int(focal)
+    else:
+        label = instruction.split('-')[0]
+        boxnum = hash(label)
+        if label in boxes[boxnum].keys():
+            boxes[boxnum].pop(label)
+
+
+answer = 0
+for i, box in enumerate(boxes):
+
+    if len(box) == 0:
+        continue
+
+    for slot, (label, focal) in enumerate(box.items()):
+        answer += (i+1) * (slot+1) * focal
 
 print(answer)
+
+
+
+
