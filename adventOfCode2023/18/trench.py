c48aec49ecca918ee266d960b3e9e2531a48f4ac
Henrik Persson <henrik.persson@plejd.com>
2023-12-21 19:36:25 +0100

    aoc 18 1

--- adventOfCode2023/18/trench.py
+++ adventOfCode2023/18/trench.py
@@ -1,10 +1,10 @@
-from example import dig_instructions
+from input import dig_instructions
 
 # Ok, so lets paint it
 import pygame
 
 # figure out the canvas
-S = SCALING = 100
+S = SCALING = 1
 
 top = 0
 left = 0
@@ -21,24 +21,24 @@ for dig in dig_instructions:
             width  = len * S
             height = S
             left  -= width
-            coords.append((x-len-1, y))
+            coords.append((x-len, y))
         case 'R':
             width  = len * S
             height = S
             left  += S
-            coords.append((x+len+1, y))
+            coords.append((x+len, y))
         case 'U':
             width  = S
             height = len * S
             top   -= height
             left  -= S
-            coords.append((x, y-len-1))
+            coords.append((x, y-len))
         case 'D':
             width  = S
             height = len * S
             top   += S
             left  -= S
-            coords.append((x, y+len+1))
+            coords.append((x, y+len))
 
     trenches.append(pygame.Rect(left, top, width, height))
 
@@ -63,15 +63,32 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 clock = pygame.time.Clock()
 
+perimeter_area = 0
 for i, trench in enumerate(trenches):
     color = dig_instructions[i][2]
     pygame.draw.rect(screen, color, trench.move(x_offset, y_offset))
     pygame.display.flip()
 
+    perimeter_area += trench.height/S * trench.width/S
+
+print(f'perimeter_area: {perimeter_area}')
+
 area = 0
 import numpy as np
-for coord in enumerate(coords):
-    pass # TODO np.linalg.det
+#coords = [(1,6), (3,1), (7,2), (4,4), (8,5)]
+coords = np.array(coords)
+area = 0
+for i in range(1, coords.shape[0]):
+    a = np.linalg.det(coords[i-1:i+1])
+    area += a
+area += np.linalg.det([coords[-1], coords[0]])
+
+print(f'inside area: {area/2}')
+
+# Inside area is the coordinates smack dab in the middle of the squares that the
+# trenches are made up of. That means the inside perimeter includes half of the perimeter area.
+# and apparently +1 because the starting hole doesn't count
+print(f'polygon area: {area/2 + perimeter_area/2 + 1}')
 
 while True:
     event = pygame.event.poll() # get event immediately, if any
