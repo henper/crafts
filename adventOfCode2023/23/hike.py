ebda5b1f7d0109fb2bd4f44eea8f65f82ad3bd86
Henrik Persson <henrik.persson@plejd.com>
2024-01-06 14:42:00 +0100

    aoc 23 2 wip

--- adventOfCode2023/23/hike.py
+++ adventOfCode2023/23/hike.py
@@ -1,7 +1,70 @@
 from example import trails
+from itertools import permutations, pairwise
+import pygame
 
 rows = len(trails)
 cols = len(trails[0])
+
+start = (1, 0)
+goal  = (cols-2, rows-1, 0)
+
+# figure out the canvas
+S = SCALING = 25
+HEIGHT = rows * S
+WIDTH  = cols * S
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+level = pygame.Surface((WIDTH, HEIGHT))
+
+for y in range(rows):
+    for x in range(cols):
+        if trails[y][x] == '#':
+            woods = pygame.Rect(x*S, y*S, S, S)
+            pygame.draw.rect(level, pygame.Color('chartreuse4'), woods)
+level = level.convert()
+
+screen.blit(level, (0,0))
+pygame.display.flip()
+
+hiker_colors = [
+    pygame.Color('firebrick1'),
+    pygame.Color('firebrick2'),
+    pygame.Color('firebrick3'),
+    pygame.Color('firebrick4'),
+    pygame.Color('black')
+]
+
+clock = pygame.time.Clock()
+def event_loop(nobreak = False):
+    while True:
+        event = pygame.event.poll() # get event immediately, if any
+        # Allow for ways to exit the application
+        if event.type == pygame.QUIT: # closing the (proverbial) window
+            pygame.quit(); exit()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_c and pygame.key.get_mods() & pygame.KMOD_CTRL:
+            pygame.quit(); exit()
+
+        if not nobreak and event.type == pygame.MOUSEBUTTONDOWN:
+            break
+
+        clock.tick(60)
+
+def visrep(paths, dead):
+    for path in paths:
+        if len(path) >= len(hiker_colors):
+            for i in range(1,len(hiker_colors)+1):
+                x, y, _ = path[-i]
+                hiker = pygame.Rect(x*S, y*S, S, S)
+                pygame.draw.rect(screen, hiker_colors[i-1], hiker)
+
+
+    for path in dead:
+        x, y, _ = path[-1]
+        hiker = pygame.Rect(x*S, y*S, S, S)
+        pygame.draw.rect(screen, pygame.Color('firebrick4'), hiker)
+    pygame.display.flip()
+    event_loop()
 
 from numpy import array
 north = array(( 0,-1))
@@ -24,6 +87,8 @@ def directions(pos):
     s = tuple(south + array(pos))
     e = tuple(east  + array(pos))
     w = tuple(west  + array(pos))
+
+    return [n, e, w, s]
 
     # if we're on an icy slope, there is no way up (well, down since we're backtracking)
     x,y = pos
@@ -54,8 +119,6 @@ def mapper(paths: list, dead: list):
         step = [(x,y) for x,y,_ in path]
         dirs = list(filter(lambda dir: dir not in step, dirs))
 
-        #dirs = list(filter(lambda dir: longer(dir,c), dirs))
-
         # dont venture off into the woods
         dirs = list(filter(lambda dir: passable(dir), dirs))
 
@@ -65,18 +128,20 @@ def mapper(paths: list, dead: list):
 
         new = True # we've taken a step in one or more directions
 
-        # if we're merely continuing on the path, then add this one step
-        if len(dirs) >= 1:
-            x,y = dirs[0]
-            path.append((x, y, c + 1))
-            fro.append(path)
-
         # at this point we need to clone ourselves and continue in more than one direction
         for i in range(1, len(dirs)):
             x,y = dirs[i]
             clone = path.copy()
             clone.append((x, y, c + 1))
             fro.append(clone)
+
+        # if we're merely continuing on the path, then add this one step
+        if len(dirs) >= 1:
+            x,y = dirs[0]
+            path.append((x, y, c + 1))
+            fro.append(path)
+
+
 
     return new, fro, dead
 
@@ -97,25 +162,62 @@ def pretty_print(paths, dead):
 
     print('')
 
-
 # start from the end
-paths = [[(cols-2, rows-1, 0)]]
+paths = [[goal]]
 
 # each mapper fills in the squares directly reachable from the current squares not yet mapped
 new, paths, dead = mapper(paths, [])
 
-pretty_print(paths, dead)
+visrep(paths, dead)
 while(new): # stop when we can't reach any more squares
     new, paths, dead = mapper(paths, dead)
-    #pretty_print(paths, dead)
+    visrep(paths, dead)
 
-pretty_print(paths, dead)
+    continue
+
+    # Run ahead and kill off hikers when any one detects the footprints of another
+    for pairs in permutations(paths, 2):
+        killer, victim = pairs
+
+        last_steps = [(x,y) for x,y,_ in killer[-2:]]
+        steps = [(x,y) for x,y,_ in victim]
+        for two_step in pairwise(steps):
+            if list(two_step) == last_steps:
+
+                x,y,k = killer[-1]
+                _,_,v =victim[steps.index((x,y))]
+
+                if k > v:
+                    # killer finds and shoots his victim
+                    paths.remove(victim)
+                    dead.append(victim)
+                else:
+                    print('tf')
+
 
 journey_lengths = []
 for path in dead:
     x,y,c = path[-1]
-    if x == 1 and y == 0:
+    if (x,y) == start:
         journey_lengths.append(c)
 
 print(journey_lengths)
 print(max(journey_lengths))
+
+
+# the ones that made it out without being killed or lost in the woods
+def at_start(pos):
+    x,y,_ = pos[-1]
+    return start == (x,y)
+
+actually_alive = list(filter(lambda path: at_start(path), dead))
+life_lived = [len(hiker) for hiker in actually_alive]
+print(max(life_lived))
+
+longest_hike = actually_alive[life_lived.index(max(life_lived))]
+for x,y,_ in longest_hike:
+        hiker = pygame.Rect(x*S, y*S, S, S)
+        pygame.draw.rect(screen, pygame.Color('gold'), hiker)
+
+pygame.display.flip()
+event_loop(True)
