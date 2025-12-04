c3d0a77406a6d25439620fff32a527519a3518c2
Henrik Persson <henrik.persson@plejd.com>
2025-12-04 15:49:47 +0100

    AoC 25 4 2

--- adventOfCode2025/4/printing_dept.py
+++ adventOfCode2025/4/printing_dept.py
@@ -1,12 +1,14 @@
 import numpy as np
 
-from example import grid
+from input import grid
 
 # read grid into matrix
-topology = []
-for line in grid:
-    topology.append(list(line))
-topology = np.array(topology)
+def grid2mat(grid):
+    topology = []
+    for line in grid:
+        topology.append(list(line))
+    topology = np.array(topology)
+    return topology
 
 def directions(pos):
     north = tuple(np.array((-1, 0)) + np.array(pos))
@@ -28,28 +30,42 @@ def occupied(dir):
     return topology[dir] == '@'
 
 
-grabbable = 0
+def grabbable(topology):
+    count_grabbable = 0
+    new_topo = topology.copy()
+    for y in range(topology.shape[0]):
+        for x in range(topology.shape[1]):
+            if topology[y,x] == '.' or topology[y,x] == 'x':
+                #print('.', end='')
+                continue
 
-for y in range(topology.shape[0]):
-    for x in range(topology.shape[1]):
-        if topology[y,x] == '.':
-            print('.', end='')
-            continue
+            neighbors = directions((y,x))
 
-        neighbors = directions((y,x))
+            neighbors = list(filter(lambda pos: inside(pos), neighbors))
 
-        neighbors = list(filter(lambda pos: inside(pos), neighbors))
+            neighbors = list(filter(lambda dir: occupied(dir), neighbors))
 
-        neighbors = list(filter(lambda dir: occupied(dir), neighbors))
+            if len(neighbors) >= 4:
+                pass
+                #print('@', end='')
+            else:
+                new_topo[y,x] = 'x'
+                #print('x', end='')
 
-        if len(neighbors) >= 4:
-            print('@', end='')
-        else:
-            print('x', end='')
-            grabbable += 1
+                count_grabbable += 1
+        #print()
+    return count_grabbable, new_topo
 
+topology = grid2mat(grid)
 
+count_grabbable, new_topo = grabbable(topology)
+print(f"Grabbable rolls: {count_grabbable}")
 
-    print()
+tot_grabbable = count_grabbable
 
-print(f"Grabbable rolls: {grabbable}")
+while count_grabbable > 0:
+    topology = new_topo
+    count_grabbable, new_topo = grabbable(topology)
+    tot_grabbable += count_grabbable
+    print(f"Grabbable rolls: {count_grabbable}")
+print(f"Total grabbable rolls: {tot_grabbable}")
