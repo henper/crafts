75acc2f7a6808e3dfbe08fdd2e9bba744cf32020
Henrik Persson <henrik.persson@plejd.com>
2025-12-11 22:36:29 +0100

    AoC 25 10 2 somewhat slightly even more optimized

--- adventOfCode2025/10/factory.py
+++ adventOfCode2025/10/factory.py
@@ -1,7 +1,7 @@
 from itertools import combinations_with_replacement
-from numpy import array, all, any
+from numpy import array, all, any, where, count_nonzero
 
-from example import machines
+from input import machines
 
 answer = 0
 
@@ -41,7 +41,9 @@ print(answer)
 answer = 0
 # part 2
 for machine in machines:
+    #machine = machines[20]
     _, buttons, joltage_requirements = machine
+    print(joltage_requirements)
 
     result = [0] * len(joltage_requirements) # no joltage requirements would mean that no clicks are needed
 
@@ -57,11 +59,11 @@ for machine in machines:
         cache = new_cache
         new_cache = {}
 
-        print(f"cache length: {len(cache)}")
+        print(f"[{clicks:2}] cache length: {len(cache)}")
 
         clicks += 1
         clicked_combos = combinations_with_replacement(buttons, clicks)
-        clicked_combos = list(clicked_combos) # comment me!
+        #clicked_combos = list(clicked_combos) # comment me!
         for clicked in clicked_combos:
 
             # fetch the result from the previously built cache
@@ -76,17 +78,43 @@ for machine in machines:
             for activation in click:
                 result[activation] += 1
 
-            # store the result of the clicks in the cache for the next number of clicks
             diff = joltage_requirements - result
-            if all(diff >= 0):
-                new_cache[clicked] = result
 
             if all(diff == 0):
                 break
 
+            possible = all(diff >= 0) # ensure this combination of clicks have not exceeded any requirement
+
+            # check if two or more joltages are met with the current combination of clicks
+            # and then if there is any chance of completing the other requirements with the
+            # buttons available
+            if possible and len(diff) - count_nonzero(diff) > 0:
+                met_indices   = set(where(diff == 0)[0])
+                unmet_indices = set(where(diff > 0)[0])
+                impossible = []
+                for unmet_idx in unmet_indices:
+                    impossible.append(True)
+                    # see if there is a button that can affect the unmet requirement, without affecting the met ones
+                    for button in buttons:
+                        if type(button) == int:
+                            button = (button,)
+                        if unmet_idx not in button:
+                            continue
+                        if not set(button).intersection(met_indices):
+                            impossible[-1] = False
+                            break # possible to meet this requirement with this button
+                if all(impossible):
+                    possible = False
+
+            # store the result of the clicks in the cache for the next number of clicks
+            if possible:
+                new_cache[clicked] = result
+
+
 
     answer += clicks
-    print(clicks)
+    print(f'clicks to solve: {clicks}')
     #print(clicked)
+    #break
 
 print(f'answer: {answer}')
