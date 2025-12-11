1cb8236c9a0f3439daeb7227321bd843bcaa3dce
Henrik Persson <henrik.persson@plejd.com>
2025-12-11 17:53:11 +0100

    AoC 25 10 2 unopt

--- adventOfCode2025/10/factory.py
+++ adventOfCode2025/10/factory.py
@@ -4,6 +4,7 @@ from input import machines
 
 answer = 0
 
+# part 1
 for machine in machines:
     indicators, buttons, _ = machine
 
@@ -32,6 +33,39 @@ for machine in machines:
 
     answer += clicks
     #print(clicks)
+    #print(clicked)
+
+print(answer)
+
+answer = 0
+# part 2
+for machine in machines:
+    _, buttons, joltage_requirements = machine
+
+    result = [0] * len(joltage_requirements) # no joltage requirements would mean that no clicks are needed
+
+    clicks = 0
+
+    while result != joltage_requirements:
+        clicks += 1
+
+        clicked_combos = combinations_with_replacement(buttons, clicks)
+        clicked_combos = list(clicked_combos) # comment me!
+        for clicked in clicked_combos:
+
+            for click in clicked:
+                if type(click) == int:
+                    click = (click,)
+                for activation in click:
+                    result[activation] += 1
+
+            if result == joltage_requirements:
+                break
+
+            result = [0] * len(joltage_requirements)
+
+    answer += clicks
+    print(clicks)
     #print(clicked)
 
 print(answer)
